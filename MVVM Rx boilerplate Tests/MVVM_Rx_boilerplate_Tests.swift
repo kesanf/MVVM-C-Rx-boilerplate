1fe1abf627fe6f4a3dae25967a58b0f6e5a2c071
Faruk Kesan <kesan.faruk@gmail.com>
2019-02-27 17:14:34 +0100

    add tests

---
+++ MVVM Rx boilerplate Tests/MVVM_Rx_boilerplate_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  MVVM_Rx_boilerplate_Tests.swift
+//  MVVM Rx boilerplate Tests
+//
+//  Created by Faruk Kesan on 27/02/2019.
+//  Copyright © 2019 Faruk. All rights reserved.
+//
+
+import XCTest
+
+class MVVM_Rx_boilerplate_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
