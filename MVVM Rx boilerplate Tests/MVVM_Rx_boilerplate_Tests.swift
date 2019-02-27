4b1fe94ac7d053f9213726621a1d3082d8cb73dd
Faruk Kesan <kesan.faruk@gmail.com>
2019-02-27 17:49:46 +0100

    add fastlane

--- MVVM Rx boilerplate Tests/MVVM_Rx_boilerplate_Tests.swift
+++ MVVM Rx boilerplate Tests/MVVM_Rx_boilerplate_Tests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class MVVM_Rx_boilerplate_Tests: XCTestCase {
+class MVVMRxboilerplateTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
